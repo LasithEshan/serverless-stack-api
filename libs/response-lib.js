6d83c80cbe4e8ab8c84d1bf4082afa348c9f9085
lasitheshan <lasitheshan1994@gmail.com>
2019-10-11 18:08:34 +0530

    create, get, getall functions added

---
+++ libs/response-lib.js
@@ -0,0 +1,18 @@
+function buildResponse(status, body){
+    return {
+        statusCode: status,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify(body)
+    };
+}
+
+export function success(body){
+    return buildResponse(200, body);
+}
+
+export function failure(body){
+    return buildResponse(500, body);
+}
